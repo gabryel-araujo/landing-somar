dfb8b3f1d77eee8bc98041764f1de76555c95fe1
gabryel <gabryelaraujo71@gmail.com>
2025-04-29 10:46:15 -0300

    fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,7 +20,7 @@ export const Hero = () => {
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
               <a
-                href="https://appsomarequipes.vercel.app"
+                href="https://appsomarequipes.vercel.app/dashboard"
                 target="_blank"
                 rel="noopener"
                 className="px-8 py-4 text-lg font-medium text-center text-white bg-sky-600 rounded-md "
